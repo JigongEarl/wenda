e008f0a85e465766be11adcc19fe8bbcc592c6d4
10033 <1003347757@qq.com>
2025-10-24 14:56:33 +0800

    测试一下

--- src/main/java/com/nowcoder/WendaApplication.java
+++ src/main/java/com/nowcoder/WendaApplication.java
@@ -16,4 +16,5 @@ public class WendaApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(WendaApplication.class, args);
 	}
+
 }
--- src/test/java/com/nowcoder/test01.java
+++ src/test/java/com/nowcoder/test01.java
@@ -4,4 +4,10 @@ public class test01 {
 	public static void main(String[] args) {
 		System.out.println("hahaaaaabbbb");
 	}
+
+	public static void test02() {
+
+		System.out.println("hahaaaaabbbb");
+
+	}
 }
